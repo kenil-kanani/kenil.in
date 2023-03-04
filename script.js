a9c9e7d3e837202e69329a6fab98114f089a5cb5
kenil-kanani <kenilkananigecbvn@gmail.com>
2023-03-04 18:29:54 +0530

    First Commit, 4 file or folder Added

---
+++ script.js
@@ -0,0 +1,17 @@
+let sun = document.querySelector(".sun")
+let moon = document.querySelector(".moon")
+
+sun.addEventListener("click" , () => {
+    sun.classList.toggle("fa-sun")
+    sun.classList.toggle("fa-moon")
+    if (sun.classList.contains("fa-sun")) {
+        document.body.style.backgroundColor = "#11111b"
+        document.body.style.color = "White"
+    }
+    else{
+        document.body.style.backgroundColor = "White"
+        document.body.style.color = "#11111b"
+
+    }
+})
+
