25f9b976857d41e4873e471fa100fa5acfc1c7f9
kenil-kanani <kenilkananigecbvn@gmail.com>
2023-03-10 13:36:41 +0530

    remove some bugs

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
+// Theam JS
+
 let sun = document.querySelector(".sun")
 let moon = document.querySelector(".moon")
+let lang = document.querySelectorAll(".lang")
+let project = document.querySelectorAll(".project")
+let a_nav = document.body.querySelectorAll(".menu-list a");
 
 sun.addEventListener("click" , () => {
     sun.classList.toggle("fa-sun")
     sun.classList.toggle("fa-moon")
+        lang.forEach(element => {
+            element.classList.toggle("theam")     
+        });
+        project.forEach(element => {
+            element.classList.toggle("theam")     
+        });
     if (sun.classList.contains("fa-sun")) {
         document.body.style.backgroundColor = "#11111b"
         document.body.style.color = "White"
+        a_nav.forEach((el) => {
+            el.style.color = "White";
+        })
     }
     else{
         document.body.style.backgroundColor = "White"
         document.body.style.color = "#11111b"
+        a_nav.forEach((el) => {
+            el.style.color = "Black";
+        })
 
     }
 })
 
-// Button Animation
+// toggle class
+function reuse() {
+    document.body.querySelector(".nav_menu").classList.toggle("hig");
+    document.body.querySelector(".change-fa").classList.toggle("fa-bars");
+    document.body.querySelector(".change-fa").classList.toggle("fa-xmark");
+}
+document.body.querySelector(".bars").addEventListener("click" , reuse)
 
-const btn_arrow = document.querySelector(".arrow");
-const btn = document.querySelector("#see-my-eork-btn");
-btn.addEventListener("onmouseenter",()=>{
-    btn_arrow.style.right = "100px";
-})
+let a = document.body.querySelectorAll(".nav_menu a");
 
+    a[0].addEventListener("click",reuse);
+    a[1].addEventListener("click",reuse);
+    a[2].addEventListener("click",reuse);
+    a[3].addEventListener("click",reuse);
 
 
 
